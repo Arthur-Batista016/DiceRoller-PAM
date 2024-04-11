fb8ef007aa6eb642275eb74a7c8e43dd4c1bcef6
Etec Prof. Horácio Augusto da Silveira <marcospborborema@hotmail.com>
2024-04-11 12:29:18 -0300

    feat:Prova Finalizada

---
+++ DiceRoller/DiceRoller/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RollBtn_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+    }
+
+}
